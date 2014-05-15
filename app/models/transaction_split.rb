0bfeb78b2b5a75a99b2f15dee75b1b03bc346997
Matt Light <matt.light@lightdatasys.com>
2014-05-14 23:39:16 -0700

    Add TxSplit has_many PiePieces association

--- app/models/transaction_split.rb
+++ app/models/transaction_split.rb
@@ -1,3 +1,5 @@
 class TransactionSplit < ActiveRecord::Base
   belongs_to :transaction_record
+  has_many :pie_piece_transaction_splits
+  has_many :pie_pieces, through: :pie_piece_transaction_splits
 end
