15fdd0f5121f6f1ff610721ddab7668ea659ccde
Matt Light <matt.light@lightdatasys.com>
2014-05-27 06:22:42 -0700

    Scaffold Api::V1::TransactionSplits
    rails g controller Api::V1::TransactionSplits index create update delete

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,11 @@ Rails.application.routes.draw do
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  namespace :api, :defaults => {:format => :json} do
+    namespace :v1 do
+      resources :transaction_splits, only: [:index, :create, :update, :destroy]
+    end
+  end
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
---
+++ test/helpers/api/v1/transaction_splits_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Api::V1::TransactionSplitsHelperTest < ActionView::TestCase
+end
