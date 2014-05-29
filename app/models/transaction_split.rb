75e9422c48445168b622bc8b7faa1b5a0b637b38
Matt Light <matt.light@lightdatasys.com>
2014-05-29 14:44:01 -0700

    Add a way to retrieve all transaction splits for a user

--- app/models/transaction_split.rb
+++ app/models/transaction_split.rb
@@ -2,4 +2,8 @@ class TransactionSplit < ActiveRecord::Base
   belongs_to :transaction_record
   has_many :pie_piece_transaction_splits
   has_many :pie_pieces, through: :pie_piece_transaction_splits
+  scope :user, ->(id) {
+    joins(:transaction_record)
+    .where('transaction_records.user_id = ?', id)
+  }
 end
