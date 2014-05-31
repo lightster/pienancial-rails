2e9743aa64c02693338c0e5d083fedcd30ccd332
Matt Light <matt.light@lightdatasys.com>
2014-05-31 09:52:35 -0700

    Add user scope to pie

--- app/models/pie.rb
+++ app/models/pie.rb
@@ -2,4 +2,7 @@ class Pie < ActiveRecord::Base
   belongs_to :user
   validates_uniqueness_of :title, scope: :user_id
   has_many :pie_pieces
+  scope :user, ->(id) {
+    where(:user_id => id)
+  }
 end
