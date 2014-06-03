336adaa694c0499268f7c108a42ae5512e856083
Matt Light <matt.light@lightdatasys.com>
2014-06-03 08:14:50 -0700

    Implement 'DELETE /api/v1/pies/:id'

--- app/controllers/api/v1/pies_controller.rb
+++ app/controllers/api/v1/pies_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::PiesController < ApplicationController
+  before_action :set_pie, only: [:destroy]
+
   def index
     @pies = Pie.all.user(1)
   end
@@ -18,10 +20,16 @@ class Api::V1::PiesController < ApplicationController
   def update
   end
 
-  def delete
+  def destroy
+    @pie.destroy
+    head :no_content
   end
 
   private
+    def set_pie
+      @pie = Pie.find_by! id: params[:id], user_id: get_current_user.id
+    end
+
     def pie_params
       new_params = params.permit(
         :title,
