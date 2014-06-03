ca0f3644580e9e7a48793c57b62dbd3e57abe31b
Matt Light <matt.light@lightdatasys.com>
2014-06-02 18:04:29 -0700

    Implement create pies

--- app/controllers/api/v1/pies_controller.rb
+++ app/controllers/api/v1/pies_controller.rb
@@ -4,6 +4,15 @@ class Api::V1::PiesController < ApplicationController
   end
 
   def create
+    @pie = Pie.new(pie_params.merge!(
+      :user_id => get_current_user.id
+    ))
+
+    if @pie.save
+      render json: @pie, status: :created
+    else
+      render json: @pie.errors, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -11,4 +20,12 @@ class Api::V1::PiesController < ApplicationController
 
   def delete
   end
+
+  private
+    def pie_params
+      new_params = params.permit(
+        :title,
+        :is_required
+      )
+    end
 end
